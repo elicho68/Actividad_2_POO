0b6d48aaa0b629adc92cef93a4985fb0c140e59c
elicho68 <elicho68@gmail.com>
2024-02-25 08:49:17 -0600

    segundo_commit

--- Persona.cpp
+++ Persona.cpp
@@ -1,19 +1,26 @@
 #include <iostream>
 using namespace std;
+//clases de personas 
 class Persona{
 	//atributos
-	protected : string nombres, apellidos, direccion;
+	protected : string nombres, apellidos, direccion, fecha_nacimiento;
 				int telefono;
+				
 	//constructor
 	protected : 
 			Persona(){
 			} 
-			Persona(string nom, string ape, string dir, int tel){
+			Persona(string nom, string ape, string dir, int tel,string fn){
 				nombres=nom;
 				apellidos=ape;
 				direccion=dir;
+				fecha_nacimiento=fn;
 				telefono=tel;
 			} 
-	//metodo
+	//metodo	
+	void crear();
+	void leer();
+	void actualizar();
 	void mostrar(); 
-}; 
+}
+; 
